cc1e0b015c0e00dff17725f482b60e4fbf79890b
RoeiBachar <roeibachar972@gmail.com>
2023-04-22 14:05:08 +0300

    first

---
+++ src/Components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import "./Main.css";
+import srca from '../../img/2-removebg-preview.png';
+
+function Main(): JSX.Element {
+    return (
+        <div className="Main">
+			        <img src={srca} />
+
+        </div>
+    );
+}
+
+export default Main;
